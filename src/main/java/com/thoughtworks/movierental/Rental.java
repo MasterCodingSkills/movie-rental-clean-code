d4288b3ea1c134b8d482fad37ae4ba37a00a26e7
MasterCodingSkills <56904368+MasterCodingSkills@users.noreply.github.com>
2020-07-05 17:25:04 +0530

    Remove unnecessary getter calls from Rental amount method

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -20,19 +20,19 @@ public class Rental {
 
   public double amount() {
     double amount = 0;
-    switch (getMovie().getPriceCode()) {
+    switch (movie.getPriceCode()) {
       case Movie.REGULAR:
         amount += 2;
-        if (getDaysRented() > 2)
-          amount += (getDaysRented() - 2) * 1.5;
+        if (daysRented > 2)
+          amount += (daysRented - 2) * 1.5;
         break;
       case Movie.NEW_RELEASE:
-        amount += getDaysRented() * 3;
+        amount += daysRented * 3;
         break;
       case Movie.CHILDRENS:
         amount += 1.5;
-        if (getDaysRented() > 3)
-          amount += (getDaysRented() - 3) * 1.5;
+        if (daysRented > 3)
+          amount += (daysRented - 3) * 1.5;
         break;
     }
     return amount;
